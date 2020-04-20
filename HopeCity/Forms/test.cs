43306b571d4c600ae0b4573a8570c1e648382c82
Mohamed Alaa <mohmed_2006_mmmm@hotmail.com>
2020-04-20 17:34:57 +0200

    add Question form

--- HopeCity/Forms/test.cs
+++ HopeCity/Forms/test.cs
@@ -16,5 +16,225 @@ namespace HopeCity.Forms
         {
             InitializeComponent();
         }
+
+        private void bunifuRadioButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuRadioButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label33_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuUserControl3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuUserControl3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void test_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuRadioButton28_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuUserControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label39_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label62_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label117_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label114_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label123_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ41_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ42_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuUserControl4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ14_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ15_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ16_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ17_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ18_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ21_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ22_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ23_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ24_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ25_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ26_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ27_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ28_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ29_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ30_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ31_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ33_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ34_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ35_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ36_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ39_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ43_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ44_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ45_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQ9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
